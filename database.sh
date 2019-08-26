bc03ccf7e302c3a838dc6a2f27a1a34b5eccfcfc
thiagoSLV <thiago.d.silv@gmail.com>
2019-08-26 13:29:42 -0300

    Saved development configuration

--- database.sh
+++ database.sh
@@ -2,7 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER thiago;
+	CREATE USER thiago WITH PASSWORD 'root';
 	CREATE DATABASE wiser_med;
 	GRANT ALL PRIVILEGES ON DATABASE wiser_med TO thiago;
 EOSQL
